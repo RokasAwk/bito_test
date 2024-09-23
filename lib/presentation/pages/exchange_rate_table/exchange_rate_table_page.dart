a22dfb07ed075f3d1359b364c439bbc468954663
Rokas <icestar6215@gmail.com>
2024-09-24 00:07:00 +0800

    feat: update ExchangeRateTablePage layout and fetch data at beginning

--- lib/presentation/pages/exchange_rate_table/exchange_rate_table_page.dart
+++ lib/presentation/pages/exchange_rate_table/exchange_rate_table_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/annotations.dart';
+import 'package:bito_test/domain/entity/currency_item.dart';
 import 'package:bito_test/presentation/pages/exchange_rate_table/exchange_rate_table_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../di_providers/di_provider.dart';
@@ -19,27 +21,82 @@ class ExchangeRateTablePage extends ConsumerStatefulWidget {
 
 class _ExchangeRateTablePageState extends ConsumerState<ExchangeRateTablePage> {
   @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await ref
+          .read(exchangeRateTableStateNotifierProvider.notifier)
+          .fetchData();
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     ExchangeRateTableState state =
         ref.watch(exchangeRateTableStateNotifierProvider);
-    ExchangeRateTableNotifier notifier =
-        ref.read(exchangeRateTableStateNotifierProvider.notifier);
 
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
             title: _buildAppBarTitle(),
+            bottom: _buildAppBarSubTitle(),
           ),
-          body: Container(
-            alignment: Alignment.topCenter,
-            padding: const EdgeInsets.only(top: 32),
-            child: Column(
-              children: [],
-            ),
-          )),
+          body: ListView.builder(
+              itemCount: state.currencyList.length,
+              itemBuilder: (context, index) {
+                return _buildItemTile(item: state.currencyList[index]);
+              })),
     );
   }
+}
+
+Widget _buildItemTile({
+  required CurrencyItem item,
+}) {
+  return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Image.network(
+                item.iconUrl,
+                width: 25,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Text('${item.currency.isoCode} / TWD')
+            ],
+          ),
+          Text('${item.twdPrice}'),
+        ],
+      ));
+}
+
+PreferredSize _buildAppBarSubTitle() {
+  return PreferredSize(
+      preferredSize: const Size.fromHeight(40),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        height: 35,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildSubTitle(text: 'Currency'),
+            _buildSubTitle(text: 'Price'),
+          ],
+        ),
+      ));
+}
+
+Widget _buildSubTitle({required String text}) {
+  return Text(
+    text,
+    style: const TextStyle(color: Colors.white, fontSize: 16),
+  );
 }
 
 Widget _buildAppBarTitle() {
