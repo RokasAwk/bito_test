8dd6909a78b8a696412d5fbb9fc3c423179af6eb
Rokas <icestar6215@gmail.com>
2024-09-24 00:05:27 +0800

    feat: ExchangeRateTableNotifier add fetch data function

--- lib/presentation/pages/exchange_rate_table/exchange_rate_table_notifier.dart
+++ lib/presentation/pages/exchange_rate_table/exchange_rate_table_notifier.dart
@@ -1,3 +1,5 @@
+import 'package:bito_test/domain/usecases/get_pairs_usecase.dart';
+
 import 'exchange_rate_table_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,30 +9,25 @@ abstract class ExchangeRateTableNotifier
     extends StateNotifier<ExchangeRateTableState> {
   ExchangeRateTableNotifier(super.state);
 
-  void goToExchangeRateTablePage();
-  void goToConversionPage();
-  void goToSelectionPage();
+  Future<void> fetchData();
 }
 
 class ExchangeRateTableNotifierImpl extends ExchangeRateTableNotifier {
+  final GetPairsUseCase getPairsUseCase;
   final AppRouter appRouter;
 
   ExchangeRateTableNotifierImpl({
+    required this.getPairsUseCase,
     required this.appRouter,
   }) : super(ExchangeRateTableState.init());
 
   @override
-  void goToExchangeRateTablePage() {
-    // appRouter.push(PasswordFastLoginRoute());
-  }
-
-  @override
-  void goToConversionPage() {
-    // TODO: implement goToConversionPage
-  }
-
-  @override
-  void goToSelectionPage() {
-    // TODO: implement goToSelectionPage
+  Future<void> fetchData() async {
+    final result = await getPairsUseCase.execute(null);
+    result.when(success: (data) {
+      state = ExchangeRateTableState(currencyList: data);
+    }, error: (error) {
+      state = ExchangeRateTableState.init();
+    });
   }
 }
